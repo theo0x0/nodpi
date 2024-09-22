2b10fad7ee8cb416dbfbd45ce9c8dae8f7972112
Theo <you@example.com>
2024-09-22 14:32:31 +0500

    yaml

---
+++ check_yt_connectivity.py
@@ -0,0 +1,9 @@
+import socket
+
+s=socket.socket()
+s.settimeout(4)
+try:
+    s.connect(("rr8---sn-gvnuxaxjvh-n8ml.googlevideo.com", 443))
+    print("Блокировки по IP адресу нет")
+except:
+    print("IP адрес заблокирован")
